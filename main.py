d95e90e7a1cca3209b0dc83942c33a47dddc567e
timiolarry <38512262+RotimiOlarry@users.noreply.github.com>
2023-01-10 00:51:58 -0600

    Errors in game frame

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ import random
 #Creating the game frame 
 
 window = turtle.Screen()
-window.setup(0.6, 0,6)
+window.setup(0.6, 0.6)
 window.title("The Lunar Landing Game")
 window.bgcolor("black")
 
-height = turtle.window_height()
-width = turtle.window_width()
+height = window.window_height()
+width = window.window_width()
 
 #Setting game parameters
-num_of_stars = random.randint(80,140)
+num_of_stars = 140
 stars = turtle.Turtle()
 stars.hideturtle()
 stars.penup()
 stars.color("white")
 
 for _ in range(num_of_stars):
-    x_position = random(-width//2 , width//2)
-    y_position = random(-height//2,height//2 )
+    x_position = random.randint(-width//2 , width//2)
+    y_position = random.randint(-height//2,height//2 )
     stars.setposition(x_position,y_position)
-    stars.dot(random(2,6))
+    stars.dot(random.randint(2,6))
     
 moon = turtle.Turtle()
-moon.penuppenup()
+moon.penup()
 moon.color("slate gray")
 moon.sety(-height * 2.8)
 moon.dot(height * 5)
